4dd526db20eb0778ce09ce7f1d045990359faa4d
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-03-08 02:04:52 +0300

    returned 'r'

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if st.sidebar.button('Рассчитать'):
     try:
         model = pickle.load(open(r'D:\workplace\data_science\pycharm\mvp_workshop\xgb_grid_clf.pcl', 'rb'))
     except:
-        model = pickle.load(open('https://github.com/Vasart-ds/yandex_workshop/blob/main/xgb_grid_clf.pcl', 'rb'))
+        model = pickle.load(open(r'https://github.com/Vasart-ds/yandex_workshop/blob/main/xgb_grid_clf.pcl', 'rb'))
     result = model.predict_proba(data)[:, 1]
     result = result[0]
 
