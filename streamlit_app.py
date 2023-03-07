cba25e2a6c5980908df8ac43fec18c9105df8159
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-03-08 02:09:31 +0300

    Updated urlopen

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,9 @@ if st.sidebar.button('Рассчитать'):
         }
     ).astype('int64')
     try:
-        model = pickle.load(open('https://github.com/Vasart-ds/yandex_workshop/blob/main/xgb_grid_clf.pcl', 'rb'))
+        model = pickle.load(urlopen('https://github.com/Vasart-ds/yandex_workshop/blob/main/xgb_grid_clf.pcl', 'rb'))
     except:
-        model = pickle.load(urlopen(r'D:\workplace\data_science\pycharm\mvp_workshop\xgb_grid_clf.pcl', 'rb'))
+        model = pickle.load(open(r'D:\workplace\data_science\pycharm\mvp_workshop\xgb_grid_clf.pcl', 'rb'))
     result = model.predict_proba(data)[:, 1]
     result = result[0]
 
