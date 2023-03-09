e0e9b424a607ea89eb5168fed15ac073abc03a98
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2023-03-09 14:33:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import pickle
+import os
 
 # интерфейс
 age = st.sidebar.number_input(min_value=18, max_value=100, label='Введите ваш возраст:', key='age')
@@ -57,7 +58,8 @@ if st.sidebar.button('Рассчитать'):
     try:
         model = pickle.load(open(r'D:\workplace\data_science\pycharm\mvp_workshop\xgb_grid_clf.pcl', 'rb'))
     except:
-        model = pickle.load(open("./xgb_grid_clf.pcl", "rb"))
+        model = pickle.load(open("/xgb_grid_clf.pcl", "rb"))
+        #model = load(os.path.dirname(__file__) + '/xgb_grid_clf.pcl')
     result = model.predict_proba(data)[:, 1]
     result = result[0]
 
